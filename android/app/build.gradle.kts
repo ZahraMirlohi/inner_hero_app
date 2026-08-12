ceceb1e24d97c587b3fe55f0269ae33a75cf4425
ZahraMirlohi <Zahramirlohiiii@gmail.com>
2026-08-12 13:06:37 -0700

    توضیح تغییرات

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,7 +3,6 @@
 plugins {
     id("com.android.application")
     id("dev.flutter.flutter-gradle-plugin")
-    id("org.jetbrains.kotlin.android") version "2.0.21" apply true  // ✅ اضافه کنید
 }
 
 android {
@@ -35,13 +34,6 @@ android {
                 "proguard-rules.pro"
             )
         }
-    }
-}
-
-// ✅ kotlinOptions با سینتکس ساده
-kotlin {
-    compilerOptions {
-        jvmTarget = JvmTarget.JVM_17
     }
 }
 
