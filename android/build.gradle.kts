ebd70564b9697eeda470091e404cd68a73dfb4a5
ZahraMirlohi <Zahramirlohiiii@gmail.com>
2026-08-12 12:47:48 -0700

    توضیح تغییرات

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,6 +5,17 @@ allprojects {
     }
 }
 
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.6.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.21")
+    }
+}
+
 val newBuildDir: Directory =
     rootProject.layout.buildDirectory
         .dir("../../build")
