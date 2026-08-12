51da26a330b4ea1c1a8fa9ff7ec0c6fe6304cd66
ZahraMirlohi <Zahramirlohiiii@gmail.com>
2026-08-12 16:59:27 -0700

    توضیح تغییرات

---
+++ android/app/src/main/java/com/example/inner_hero_app/MainActivity.java
@@ -0,0 +1,9 @@
+// android/app/src/main/java/com/example/inner_hero_app/MainActivity.java
+
+package com.example.inner_hero_app;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+    // کلاس اصلی اپلیکیشن
+}
